f24136239a0d3f675b38f1007b48d53f9bbceff6
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2022-01-07 11:58:51 +0530

    Update styles for the content layout component

--- components/Layout/ContentLayout/index.styles.ts
+++ components/Layout/ContentLayout/index.styles.ts
@@ -3,7 +3,8 @@ import { styled } from '../../../stitches.config';
 export const Wrapper = styled('div', {
 	mx: 'auto',
 	my: '0rem',
-	width: '90%',
+	width: '100vw',
+	px: '2rem',
 
 	'@media only screen and (min-width:1440px)': {
 		maxWidth: '140rem'
